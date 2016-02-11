32f75e4575f1fcd75274342b5330f952a659ee07
Aman Thakral <aman.thakral@gmail.com>
2016-02-11 14:10:37 -0800

    Adding example to show ahout to make a raster catalog file

--- scripts/create_catalog.py
+++ scripts/create_catalog.py
@@ -1,7 +1,8 @@
+import os
 import argparse
 import json
-from spatial.vector import read_layer, read_geojson
-from spatial.utils import projection_from_string
+from pyspatial.vector import read_layer, read_geojson
+from pyspatial.utils import projection_from_string
 import gdal
 
 
@@ -10,7 +11,9 @@ if __name__ == "__main__":
     parser.add_argument('src', help='The source raster file, whether tiled '
                                     'or untiled')
 
-    parser.add_argument('dest', help='The output path for the json file')
+    parser.add_argument('--dest', dest="dest",
+                        help='The output path for the json file',
+                        default=None)
     parser.add_argument('--tiles', dest='tile_path', default=None,
                         help='Specify path to tiles')
 
@@ -45,8 +48,25 @@ if __name__ == "__main__":
 
     if args.grid_size is not None:
         assert args.tile_path is not None, "Must supply --tiles"
-        catalog["GridSize"] = args.grid_size
-        catalog["Path"] = args.tile_path
+        if os.path.exists(args.tile_path):
+            tiles = os.listdir(args.tile_path)
+            if len(tiles) == 0:
+                raise ValueError("%s is empty" % args.tile_path)
+
+            tile = os.path.join(args.tile_path, tiles[0])
+            ds = gdal.OpenShared(tile)
+            if ds is None:
+                raise ValueError("Unable to open file: %s" % tile)
+
+            xsize = ds.RasterXSize
+            ysize = ds.RasterYSize
+            if xsize != ysize:
+                raise ValueError("tiles must have same X and Y size")
+
+            catalog["GridSize"] = xsize
+            catalog["Path"] = args.tile_path
+        else:
+            raise ValueError("tiles path does not exist: %s" % args.tile_path)
 
     if args.index_path is not None:
         read = read_geojson if args.index_path.endswith("json") else read_layer
@@ -55,5 +75,8 @@ if __name__ == "__main__":
 
     catalog["Size"] = (xsize, ysize)
 
-    with open(args.dest, "w+b") as outf:
-        outf.write(json.dumps(catalog))
+    if args.dest is not None:
+        with open(args.dest, "w+b") as outf:
+            outf.write(json.dumps(catalog))
+    else:
+        print json.dumps(catalog)
