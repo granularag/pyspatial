877b30b23f3af9983c4ade3a617f1bb83460dffe
aman-thakral <aman.thakral@gmail.com>
2016-05-02 16:08:28 +0000

    Delete requirements-osx.sh
    No longer needed, since notes are in INSTALL.md

--- scripts/requirements-osx.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-brew install geos
-brew install spatialindex
-
-# Install gdal 2.0
-brew install https://gist.githubusercontent.com/brianreavis/261cf46b44669366df9c/raw/aa7f2f2a8a511975d7d1dae9e5acf5ac203ba969/gdal.rb
